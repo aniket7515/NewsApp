b0d2deeda14ee7000850afaea3311b4501b7c38d
Aniket <aniketdhokane2071@gmail.com>
2022-06-27 23:11:40 +0530

    Displaying Api data using useReducer

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,40 +1,35 @@
-import React ,{useEffect} from 'react'
+// import React ,{useEffect} from 'react'
+
+import React,{useEffect} from 'react'
+import {useGlobalContext} from './context'
+
 
 const Stories = () => {
-    let isLoading = true;
-
-    let API = "https://hn.algolia.com/api/v1/search?query=react";
-
-    const fetchApiData= async(url)=>{
-        try {
-            const res= await fetch(url);
-            const data=await res.json();
-            console.log(data);
-            // isLoading=false;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+  const {hits,isLoading}=useGlobalContext();
 
 
-    useEffect(() => {
-      fetchApiData(API);
-    }, [])
 
-    if(isLoading){
-        return(
-            <>
-            <h1>Loading....</h1>
-          </>
+    // let isLoading = true;
 
-        )
+
+
+    // if(isLoading){
+    //     return(
+    //         <>
+    //         <h1>Loading....</h1>
+    //       </>
+
+    //     )
      
-    }
+    // }
     
 
   return (
     <>
        <h2>My Tech News Post</h2>
+       {hits.map((curPost)=>{
+        return <h2>{curPost.title}</h2>;
+       })}
     </>
   )
 }
--- src/context.js
+++ src/context.js
@@ -2,13 +2,62 @@
 // provider  
 // consumer   lengthy process so removed it
 //so now we use usecontexthook
-import React, { useContext } from 'react';
+import React, { useContext,useReducer,useEffect } from 'react';
+import reducer from './reducer';
+
+
+
+let API = "https://hn.algolia.com/api/v1/search?";
+
+const initialState={
+    isLoading:true,
+    query:"HTML",
+    nbPages:0,
+    page:0,
+    hits:[],
+};
+
+
 const AppContext = React.createContext();
 
 // TO create a provider function
 
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value={"Aniket Dhokane"}>{children}</AppContext.Provider>
+    // we will be using usereducer instead of usestate to manage state  
+    // const [state,setstate]=useState(initialState);
+
+    const [state,dispatch] = useReducer(reducer,initialState);
+
+
+    const fetchApiData= async(url)=>{
+        dispatch({type: "SET_LOADING"})
+
+
+        try {
+            const res= await fetch(url);
+            const data=await res.json();
+            console.log(data);
+            dispatch({
+                type: "GET_STORIES",
+                payload:{
+                    hits:data.hits,
+                    nbPages:data.nbPages,
+                }
+            
+            })
+            // isLoading=false;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    useEffect(() => {
+      fetchApiData(`${API}query=${state.query}&page=${state.page}`);
+    }, [])
+
+
+    return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
 
 }
 
